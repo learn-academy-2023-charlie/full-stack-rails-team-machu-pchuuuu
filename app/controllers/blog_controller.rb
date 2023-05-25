d9f5b8504b5a8088e7d5cb9ab5a84c543c926a9e
henrike32 <henrique.lambertucci@gmail.com>
2023-05-25 15:22:04 -0400

    added show

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,4 +3,10 @@ class BlogController < ApplicationController
       def index
         @blogs = Blog.all
       end
+
+      def show
+        @show = Blog.find(params[:id])
+      end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 
 get '/blogs' => 'blog#index'
+get '/show/:id' => 'blog#show', as: 'show'
 end
